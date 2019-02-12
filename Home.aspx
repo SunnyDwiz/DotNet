c085ca950245d29cfff572d6425c132af97612d1
SunnyDwiz <Sunnydwiz@gmail.com>
2019-02-12 14:41:17 +0530

    dot net form with validation

---
+++ Home.aspx
@@ -0,0 +1,73 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="DotNet.Home" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="Content/bootstrap.css" rel="stylesheet" />
+    <script src="Scripts/jquery-1.9.1.js"></script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="container-fluid">
+            <div class="col-md-6 col-lg-push-3">
+                <div class="panel panel-success">
+                    <div class="panel panel-heading">
+                        <div class="panel-title">Employee Registration</div>
+                    </div>
+                    <div class="panel panel-body">
+                        <div class="form-horizontal">Employee Name<asp:TextBox ID="txtName" runat="server" CssClass="form-control" required="true"></asp:TextBox></div>
+                        <asp:RegularExpressionValidator ID="rgexName" runat="server" CssClass="label-danger" SetFocusOnError="true" ControlToValidate="txtName" ErrorMessage="Please enter name only No special charectar" ValidationExpression="^[a-zA-Z]+$"></asp:RegularExpressionValidator>
+                        <div class="form-horizontal">E-Mail<asp:TextBox ID="txtMail" runat="server" CssClass="form-control" required="true"></asp:TextBox></div>
+                        <asp:RegularExpressionValidator ID="rgexEmail" runat="server" CssClass="label-danger" SetFocusOnError="true" ControlToValidate="txtMail" ErrorMessage="Please Enter Valid Email ID" ValidationExpression="\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*" Visible="true"></asp:RegularExpressionValidator>
+                        <div class="form-horizontal">Phone<asp:TextBox ID="txtMobile" runat="server" CssClass="form-control" required="true"></asp:TextBox></div>
+                        <asp:RegularExpressionValidator ID="rgexMobile" runat="server" CssClass="label-danger" SetFocusOnError="true" ControlToValidate="txtMobile" ErrorMessage="Please enter 10 digit Mobile Number" ValidationExpression="^\d{10}$"></asp:RegularExpressionValidator>
+                        <div class="form-horizontal">State</div>
+                        <div class="form-horizontal">
+                            Select State<asp:DropDownList ID="drpState" required="true" CssClass="form-control" runat="server" AutoPostBack="true" OnSelectedIndexChanged="drpState_SelectedIndexChanged">
+                                <asp:ListItem>Select Any State</asp:ListItem>
+                            </asp:DropDownList>
+                            <asp:RequiredFieldValidator InitialValue="-1" ID="Req_ID" Display="Dynamic"
+                                ValidationGroup="g1" runat="server" ControlToValidate="drpState"
+                                ErrorMessage="Select one state"></asp:RequiredFieldValidator>
+                        </div>
+
+                        <div class="form-horizontal">
+                            City<asp:DropDownList ID="drpCity" CssClass="form-control" runat="server">
+                                <asp:ListItem>Select Any City</asp:ListItem>
+                            </asp:DropDownList>
+                        </div>
+                        <div class="form-horizontal has-warning">Religion</div>
+                        <div class="form-horizontal">
+                            <asp:RadioButton ID="rdHindu" runat="server" GroupName="rel" Text="Hindu" CssClass="radio-inline" />
+                            <asp:RadioButton ID="rdMuslim" runat="server" GroupName="rel" Text="Muslim" CssClass="radio-inline" />
+                            <asp:RadioButton ID="rdChrist" runat="server" GroupName="rel" Text="Christian" CssClass="radio-inline" />
+                        </div>
+                        <div class="form-horizontal has-warning">Gender</div>
+                        <div class="form-horizontal">
+                            <asp:RadioButton ID="rdMale" runat="server" GroupName="gen" Text="Male" CssClass="radio-inline" />
+                            <asp:RadioButton ID="rdFemale" runat="server" GroupName="gen" Text="Female" CssClass="radio-inline" />
+                        </div>
+                        <div class="form-horizontal has-warning">Skill</div>
+                        <div class="form-horizontal">
+                            <asp:CheckBox ID="chkCsharp" runat="server" Text="Dot Net" CssClass="checkbox-inline" />
+                            <asp:CheckBox ID="chkWeb" runat="server" Text="Web Technologies" CssClass="checkbox-inline" />
+                            <asp:CheckBox ID="chkBoostrap" runat="server" Text="Boostrap" CssClass="checkbox-inline" />
+                            <asp:CheckBox ID="chkSql" runat="server" Text="SQL Server" CssClass="checkbox-inline" />
+                        </div>
+                        <div class="form-group">
+                            <asp:Button ID="btnSubmit" runat="server" Text="Submit" CssClass=" col-sm-2 col-lg-push-4 btn btn-success" OnClick="btnSubmit_Click" />
+                        </div>
+                    </div>
+                    <div class="panel-footer">
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                
+            </div> 
+            </div>
+    </form>
+</body>
+</html>
---
+++ Home.aspx.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotNet
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        Dictionary<int, string> CityTel = new Dictionary<int, string>();
+        Dictionary<int, string> CityBr = new Dictionary<int, string>();
+        Dictionary<int, string> CityJhar = new Dictionary<int, string>();
+        Dictionary<int, string> CityMp = new Dictionary<int, string>();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+            if (!IsPostBack)
+            {
+                StateList();
+            } 
+        }
+
+        protected void StateList()
+        {
+            Dictionary<int, string> listState = new Dictionary<int, string>();
+            listState.Add(0, "Select Any State");
+            listState.Add(1, "Telangana");
+            listState.Add(2, "Jharkhand");
+            listState.Add(3, "MP");
+            listState.Add(5, "Biahr");
+            drpState.DataSource = listState;
+            drpState.DataTextField = "Value";
+            drpState.DataValueField = "Key";
+            drpState.DataBind();
+        }
+
+        protected void TelCity()
+        { // Dictionary<int, string> CityTel = new Dictionary<int, string>();
+            CityTel.Add(0, "Select City");
+            CityTel.Add(1, "hyderabad");
+            CityTel.Add(2, "Secunderabad");
+            CityTel.Add(3, "kazipet");
+            CityTel.Add(4, "Suryapet");
+            CityTel.Add(5, "Karimnagar");
+            drpCity.DataSource = CityTel;
+            drpCity.DataTextField = "Value";
+            drpCity.DataValueField = "Key";
+            drpCity.DataBind();
+
+        }
+
+        protected void MPCity()
+        {
+            // Dictionary<int, string> CityMp = new Dictionary<int, string>();
+            CityMp.Add(0, "Select City");
+            CityMp.Add(1, "Indor");
+            CityMp.Add(2, "Bhopal");
+            CityMp.Add(3, "Sagar");
+            CityMp.Add(4, "Gwalior");
+            CityMp.Add(5, "Rewa");
+            drpCity.DataSource = CityMp;
+            drpCity.DataTextField = "Value";
+            drpCity.DataValueField = "Key";
+            drpCity.DataBind();
+        }
+
+        protected void BrCity()
+        {
+            //  Dictionary<int, string> CityBr = new Dictionary<int, string>();
+            CityBr.Add(0, "Select City");
+            CityBr.Add(1, "Patna");
+            CityBr.Add(2, "Begusarai");
+            CityBr.Add(3, "Muzaffarpur");
+            CityBr.Add(4, "Gaya");
+            CityBr.Add(5, "Nalanda");
+            drpCity.DataSource = CityBr;
+            drpCity.DataTextField = "Value";
+            drpCity.DataValueField = "Key";
+            drpCity.DataBind();
+        }
+
+        protected void JHCity()
+        {
+            //Dictionary<int, string> CityJhar = new Dictionary<int, string>();
+            CityJhar.Add(0, "Select City");
+            CityJhar.Add(1, "Ranchi");
+            CityJhar.Add(2, "Jamsedpur");
+            CityJhar.Add(3, "Bokaro");
+            CityJhar.Add(4, "Dhanbad");
+            CityJhar.Add(5, "Devghar");
+            drpCity.DataSource = CityJhar;
+            drpCity.DataTextField = "Value";
+            drpCity.DataValueField = "Key";
+            drpCity.DataBind();
+
+        }
+
+        protected void drpState_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (drpState.SelectedIndex == 0)
+            {
+                Response.Write("<script>alert('Plaese Select State')</script>");
+                drpCity.SelectedIndex = 0;
+            }
+
+            else if (drpState.SelectedIndex == 1)
+            {
+                TelCity();
+            }
+            else if (drpState.SelectedIndex == 2)
+            {
+                JHCity();
+            }
+            else if (drpState.SelectedIndex == 3)
+            {
+                MPCity();
+            }
+
+            else if (drpState.SelectedIndex == 4)
+            {
+                BrCity();
+            }
+            else
+            {
+                Response.Write("<script>alert('Plaese Select State')</script>");
+            }
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            bool religion =false, skill=false, gen=false;
+            if (txtName.Text != "" && txtMobile.Text != "" && txtMail.Text != "" && drpCity.SelectedIndex != 0 && drpState.SelectedIndex != 0)
+            {
+                if (rdChrist.Checked  || rdHindu.Checked || rdMuslim.Checked)
+                {
+                    religion = true;
+                }
+                else
+                {
+                    Response.Write("<script>alert('Please select any religion!');</script>");
+                }
+                if (rdMale.Checked || rdFemale.Checked)
+                {
+                    gen = true;
+                }
+                else
+                {
+                    Response.Write("<script>alert('Please select Gender!');</script>");
+                }
+                if (chkBoostrap.Checked || chkCsharp.Checked || chkSql.Checked || chkSql.Checked)
+                {
+                    skill = true;
+                }
+                else
+                {
+                    Response.Write("<script>alert('Please select from above Skill!');</script>");
+                }
+                if (religion == true && gen == true && skill == true)
+                {
+                    btnSubmit.CssClass = "btn btn-success col-sm-2 col-lg-push-4";
+                    Response.Redirect("Welcome.aspx?Name=" + txtName.Text);
+                }
+            }
+            else
+            {
+                btnSubmit.CssClass = "btn btn-danger col-sm-2 col-lg-push-4";
+                Response.Write("<script>alert('Please Fill THe Form Correctly!');</script>");
+            }
+        }
+    }
+}
